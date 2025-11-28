60398e58b5cd4a890cc8bb429f184e10fe43509f
zgh <zgh1949@outlook.com>
2025-11-28 17:40:13 +0800

    update day009

--- day009/homework_card_game.py
+++ day009/homework_card_game.py
@@ -35,3 +35,120 @@
 - 添加更多玩家，并实现不同的发牌方式
 - 实现扑克牌游戏的图形界面
 """
+from enum import Enum
+import random
+import rich
+from rich.console import Console
+
+console = Console(highlight=False)
+
+
+class Suite(Enum):
+    hearts = (0, "♥", "[red]♥[/red]",)
+    spades = (1, "♠", "[black]♠[/black]")
+    diamonds = (2, "♦", "[red]♦[/red]")
+    clubs = (3, "♣", "[black]♣[/black]")
+
+    def __init__(self, code, sign, rich_text):
+        self.code = code
+        self.sign = sign
+        self.rich_text = rich_text
+
+
+face_list = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
+
+class Card:
+    def __init__(self, face, suite: Suite):
+        self.face = face
+        self.suite = suite
+
+    def __str__(self):
+        if self.suite and hasattr(self.suite, 'sign'):
+            return self.suite.rich_text + str(self.face)
+        else:
+            return str(self.face)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Poker:
+    def __init__(self, cards=None, current=0):
+        if cards is None or len(cards) == 0:
+            cards = []
+            for i in face_list:
+                for j in Suite:
+                    cards.append(Card(i, j))
+
+        self._cards = cards
+        self._current = current
+
+    def size(self):
+        return len(self._cards)
+
+    def shuffle(self):
+        """洗牌"""
+        current = 0
+
+        for i in range(len(self._cards)-1, 0-1, -1):
+            j = random.randint(0, i)
+            self._cards[i], self._cards[j] = self._cards[j], self._cards[i]
+
+    def next(self):
+        """发牌"""
+        if (self.has_next()):
+            self._current += 1
+            return self._cards[self._current - 1]
+        return None
+
+    def has_next(self):
+        """判断是否还有牌"""
+        return self._current < len(self._cards)
+
+    def print(self, message):
+        console.print(message)
+        for i in self._cards:
+            console.print(f"{i}", end=" ")
+        console.print()
+
+
+class Player:
+    def __init__(self, name):
+        self._name = name
+        self._cards = []
+
+    def get(self, poker: Poker):
+        """摸牌"""
+        if (poker.has_next()):
+            self._cards.append(poker.next())
+
+    def arrange(self):
+        self._cards = sorted(self._cards, key=lambda x: (face_list.index(x.face), x.suite.code))
+
+    def __str__(self):
+        return f"{self._name}:{" ".join([str(x) for x in self._cards])}"
+
+
+if __name__ == "__main__":
+    poker = Poker()
+    poker.print("这是一副新牌")
+
+    poker.shuffle()
+    poker.print("洗牌")
+
+    player1 = Player("玩家1")
+    player2 = Player("玩家2")
+    player3 = Player("玩家3")
+    player4 = Player("玩家4")
+
+    players = [player1, player2, player3, player4]
+    index = 0
+
+    while (poker.has_next()):
+        index = (index+1) % 4
+        players[index].get(poker)
+
+    for player in players:
+        player.arrange()
+        console.print(f"{player}" )
